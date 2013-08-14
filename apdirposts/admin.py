b4b03c336163da0659b32b537aade250690b1540
Lee Phillips <lee@x31>
2013-08-13 22:46:59 -0400

    Fill in initial author field.

--- apdirposts/admin.py
+++ apdirposts/admin.py
@@ -2,6 +2,20 @@ from models import Post
 from django.contrib import admin
 
 class PostAdmin(admin.ModelAdmin):
-   fields = ['author', 'title', 'pub_date', 'content']
+    def formfield_for_foreignkey(self, db_field, request, **kwargs): #[1]
+        if db_field.name == 'author':
+            kwargs['initial'] = request.user.id
+            return db_field.formfield(**kwargs)
+        return super(PostAdmin, self).formfield_for_foreignkey(
+                     db_field, request, **kwargs)
+    # def formfield_for_charfield(self, db_field, request, **kwargs):
+    #     if db_field.name == 'byline':
+    #         kwargs['initial'] = request.user.name()
+    #         return db_field.formfield(**kwargs)
+
+    fields = ['author', 'byline', 'title', 'publish', 'content']
 
 admin.site.register(Post, PostAdmin)
+
+
+#[1] http://stackoverflow.com/questions/5632848/default-value-for-user-foreignkey-with-django-admin
--- apdirposts/models.py
+++ apdirposts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Director(models.Model):
    user = models.OneToOneField(User)
@@ -12,5 +13,14 @@ class Post(models.Model):
    pub_date = models.DateTimeField('date published')
    title = models.CharField(max_length=200)
    author = models.ForeignKey(User)
+   byline = models.CharField(max_length = 500)
+   publish = models.BooleanField()
    content = models.TextField()
+   def save(self, *args, **kwargs):
+      self.pub_date =  datetime.datetime.today()
+      super(Post, self).save(*args, **kwargs)
+  
 
+   def __unicode__(self):
+      return "%s, by %s" % (self.title, self.author)
+   
