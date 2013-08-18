bbc8151cbc3c103f4ab74fdb84e5409c0d9d89f6
Lee Phillips <lee@x31>
2013-08-18 04:42:52 -0400

    Using signals to add Director object when a new user is created.

--- apdirposts/admin.py
+++ apdirposts/admin.py
@@ -1,17 +1,28 @@
 from models import Post
 from django.contrib import admin
+# from django import models
+from django.forms import ModelForm
+from django.forms import CharField
+
+# class PostAdminForm(ModelForm):
+#    byline =    ?
+#    class Meta:
+#       model = Post
 
 class PostAdmin(admin.ModelAdmin):
+    # form = PostAdminForm
     def formfield_for_foreignkey(self, db_field, request, **kwargs): #[1]
         if db_field.name == 'author':
             kwargs['initial'] = request.user.id
             return db_field.formfield(**kwargs)
         return super(PostAdmin, self).formfield_for_foreignkey(
                      db_field, request, **kwargs)
-    # def formfield_for_charfield(self, db_field, request, **kwargs):
-    #     if db_field.name == 'byline':
-    #         kwargs['initial'] = request.user.name()
-    #         return db_field.formfield(**kwargs)
+
+        # if db_field.name == 'byline':
+        #     kwargs['initial'] = 'hihihihi'
+        #     return db_field.formfield(**kwargs)
+        # return super(PostAdmin, self).formfield_for_dbfield(
+        #              db_field, **kwargs)
 
     fields = ['author', 'byline', 'title', 'publish', 'content']
 
--- apdirposts/models.py
+++ apdirposts/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from django.db.models.signals import post_save
+
+# python manage.py dumpdata <your_app> > temp_data.json
+# python manage.py reset <your_app>
+# python manage.py loaddata temp_data.json
+
 
 class Director(models.Model):
    user = models.OneToOneField(User)
    def name(self):
-      return self.get_full_name()
+      return self.user.get_full_name()
+   title = models.CharField(max_length=30)
+   nameinbyline = models.CharField(max_length = 200)
+   formalname = models.CharField(max_length = 200)
    def __unicode__(self):
       return self.name()
 
@@ -19,8 +28,14 @@ class Post(models.Model):
    def save(self, *args, **kwargs):
       self.pub_date =  datetime.datetime.today()
       super(Post, self).save(*args, **kwargs)
-  
-
    def __unicode__(self):
       return "%s, by %s" % (self.title, self.author)
-   
+
+# register a handler for the signal django.db.models.signals.post_save on the User model, and, in the handler, if created=True, create the associated user profile.
+
+def on_new_user(sender, created, instance, **kwargs):
+   if created == True:
+      # Director.objects.create(user = instance)      
+      Director(user = instance).save()
+
+post_save.connect(on_new_user, sender = User, dispatch_uid="nuser")
