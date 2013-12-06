0d708143035dee04423789b7d9b968ee34238189
Lee Phillips <lee@x31>
2013-12-05 22:43:54 -0500

    rolling on the board-only admin site

--- boarddocs/admin.py
+++ boarddocs/admin.py
@@ -3,86 +3,28 @@ from django.contrib import admin
 from django.db import models
 from django.forms import ModelForm, CharField, TextInput
 
-class AnnualreportAdmin(admin.ModelAdmin):
-    list_display = ('docname', 'pub_date')
-    list_display_links = ('title',)
-    readonly_fields = ['pub_date'] 
-    # form = PostAdminForm
-    def get_form(self, req, obj=None, **kwargs):
-        # save the logged in user 
-        self.current_user = req.user
-        return super(PostAdmin, self).get_form(req, obj, **kwargs)
-    # http://www.vimtips.org/2009/04/28/django-using-modeladmin-default-currently-logged-u/
-    def formfield_for_dbfield(self, db_field, **kwargs):
-       if db_field.name == "byline":
-          byline = Director.objects.get(user=self.current_user).nameinbyline
-          # return CharField(
-          return db_field.formfield(
-                           initial = byline,
-                           max_length = 500, 
-                           widget=TextInput(attrs={'size':'40'}))
-       return super(PostAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-  # Also see https://pypi.python.org/pypi/django-cuser/
+class BoardfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'whenUploaded', 'user')
+    list_display_links = ('name',)
+    readonly_fields = ['whenUploaded', 'name'] 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'author':
-            self.who = request.user.id
-            kwargs['initial'] = self.who
+        if db_field.name == 'user':
+            self.user = request.user.id
+            kwargs['initial'] = self.user
             return db_field.formfield(**kwargs)
-        return super(PostAdmin, self).formfield_for_foreignkey(
-                     db_field, request, **kwargs)
-
-class NoticeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'on')
-    list_display_links = ('title',)
-    readonly_fields = ['pub_date'] 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == 'author':
-            self.who = request.user.id
-            kwargs['initial'] = self.who
-            return db_field.formfield(**kwargs)
-        return super(PostAdmin, self).formfield_for_foreignkey(
+        return super(BoardfileAdmin, self).formfield_for_foreignkey(
                      db_field, request, **kwargs)
 
 
-class EventAdmin(PostAdmin):
-    list_display = ('title', 'publish', 'on', 'ebcode')
-    list_display_links = ('title',)
-    # exclude = ['author']
-    def formfield_for_dbfield(self, field, **kwargs):
-       if field.name == "byline":
-          byline = Director.objects.get(user=self.current_user).nameinbyline
-          return CharField(initial = byline,
-                           max_length = 500,
-                           widget=TextInput(attrs={'size':'40'}))
-       if field.name == 'rpost':
-          kwargs['queryset'] = Post.objects.all().order_by('-pub_date')
-          kwargs['label'] = "Related article"
-       return super(PostAdmin, self).formfield_for_dbfield(field, **kwargs)
+class AnnualreportAdmin(BoardfileAdmin):
+    list_display = ('name', 'whenUploaded', 'user', 'startDate', 'endDate')
 
-class DirectorAdmin(admin.ModelAdmin):
-   def queryset(self, request):
-      if request.user.is_superuser:
-          return Director.objects.all()
-      return Director.objects.filter(user=request.user)
-   list_display = ('user', 'nameinbyline', 'formalname')
-   list_display_links = ('user', 'nameinbyline', 'formalname')
-   exclude = ['user']
-   def get_form(self, req, obj=None, **kwargs):
-      self.current_user = req.user #Just for capturing current user
-      return super(DirectorAdmin, self).get_form(req, obj, **kwargs)
-   def formfield_for_dbfield(self, field, **kwargs):
-      if field.name == "nameinbyline":
-         return CharField(label = 'Name as byline', initial = self.current_user.get_full_name())
-   #    # elif field == "formalname":
-   #    #    return CharField(initial = self.current_user.title + ' ' + 
-   #    #                     self.current_user.get_full_name())
-      return super(DirectorAdmin, self).formfield_for_dbfield(field, **kwargs)
+class AgendaAdmin(BoardfileAdmin):
+    list_display = ('name', 'whenUploaded', 'user', 'meetingDate')
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Director, DirectorAdmin)
-admin.site.register(Illustration)
-admin.site.register(Postcategory)
-admin.site.register(Notice, NoticeAdmin)
-admin.site.register(Event, EventAdmin)
+class MinutesAdmin(BoardfileAdmin):
+    list_display = ('name', 'whenUploaded', 'user', 'meetingDate')
 
-
+admin.site.register(Annualreport, AnnualreportAdmin)
+admin.site.register(Minutes, MinutesAdmin)
+admin.site.register(Agenda, AgendaAdmin)
--- boarddocs/models.py
+++ boarddocs/models.py
@@ -6,33 +6,53 @@ from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 
 
-def getDocName(f, s):
-   return settings.MEDIA_ROOT+f.docdir+f.docnam+datetime.datetime.now().isoformat()
-
-
 class Boardfile(models.Model):
    user = models.ForeignKey(User)
-   docdir = "other/" # must have the trailing slash
    docname = "generic"
    whenUploaded = models.DateTimeField('whenUploaded') # Filled in with signal
-   upload_to = settings.MEDIA_ROOT + docdir + docname + "%F-%T" 
+   thefile = models.FileField(upload_to = "other")
    name = models.CharField('', max_length = 400, blank = True) # Filled in with signal
    covering = models.DateTimeField('Covering')
    def __unicode__(self):
       return self.name
 
-class Annualreport(Boardfile):
-   docdir = "annualreports/"
+class Annualreport(models.Model):
+   user = models.ForeignKey(User)
    docname = "AnnualReport"
+   whenUploaded = models.DateTimeField('whenUploaded') # Filled in with signal
+   thefile = models.FileField(upload_to = "annualreports")
+   name = models.CharField('', max_length = 400, blank = True) # Filled in with signal
+   startDate = models.DateField('Start Date')
+   endDate = models.DateField('End Date')
+   def __unicode__(self):
+      return self.name
 
-class Minutes(Boardfile):
-   docdir = "minutes/"
+class Minutes(models.Model):
+   user = models.ForeignKey(User)
    docname = "MeetingMinutes"
+   whenUploaded = models.DateTimeField('whenUploaded') # Filled in with signal
+   thefile = models.FileField(upload_to = "minutes")
+   name = models.CharField('', max_length = 400, blank = True) # Filled in with signal
+   meetingDate = models.DateTimeField('Meeting Date')
+   def __unicode__(self):
+      return self.name
 
-def on_boardfile_save(sender, instance, **kwargs):
+class Agenda(models.Model):
+   user = models.ForeignKey(User)
+   docname = "MeetingAgenda"
+   whenUploaded = models.DateTimeField('whenUploaded') # Filled in with signal
+   thefile = models.FileField(upload_to = "meetingAgendas")
+   name = models.CharField('', max_length = 400, blank = True) # Filled in with signal
+   meetingDate = models.DateTimeField('Meeting Date')
+   def __unicode__(self):
+      return self.name
+
+def on_annualreport_save(sender, instance, **kwargs):
    p = instance
    p.whenUploaded = datetime.datetime.now()
    p.name = p.docname + p.whenUploaded.isoformat()
 
-pre_save.connect(on_boardfile_save, sender = Boardfile, dispatch_uid="boardfilesave")
+pre_save.connect(on_annualreport_save, sender = Annualreport, dispatch_uid="apfilesave")
+pre_save.connect(on_annualreport_save, sender = Minutes, dispatch_uid="minutessave")
+pre_save.connect(on_annualreport_save, sender = Agenda, dispatch_uid="agendasave")
 
