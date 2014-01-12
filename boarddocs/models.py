4e4eb7f4d36d1a814319f59f2eef510d66a23115
Lee Phillips <lee@x31>
2014-01-12 01:03:39 -0500

    Headings and type choices in "top" pages.

--- boarddocs/models.py
+++ boarddocs/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from ap import settings
 import datetime
-from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 import os
 
