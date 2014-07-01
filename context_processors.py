bf2c33dd34e622b14158e6ac81eedb061b89a306
Lee Phillips <lee@lee-phillips.org>
2014-07-01 18:47:19 -0400

    putting static files in static dir

--- context_processors.py
+++ context_processors.py
@@ -1,7 +1,5 @@
 import settings
 def media(request):
     return {'MEDIA_URL': settings.MEDIA_URL}
-def securemedia(request):
-    return {'SECURE_MEDIA_URL': settings.SECURE_MEDIA_URL}
 def static(request):
     return {'STATIC_URL': settings.STATIC_URL}
